a60f705a5e987fcc393a49cb6d7e1739d28dfd42
jennyanndevilla <jennyann.devilla@hcdc.edu.ph>
2025-09-21 00:02:59 +0800

    Create student.js

---
+++ data/student.js
@@ -0,0 +1,78 @@
+{
+  "students": [
+    {
+      "id": 1,
+      "name": "Menard",
+      "age": 24,
+      "course": "Computer Science"
+    },
+    {
+      "id": 2,
+      "name": "Jomelyn",
+      "age": 23,
+      "course": "Cloud Computing Foundations"
+    },
+    {
+      "id": 3,
+      "name": "Micheal",
+      "age": 22,
+      "course": "Software Engineering"
+    },
+    {
+      "id": 4,
+      "name": "Emilyn",
+      "age": 21,
+      "course": "Web Development Essentials"
+    },
+    {
+      "id": 5,
+      "name": "Ann",
+      "age":19,
+      "course": "Information Technology"
+    }
+  ],
+  "courses": [
+    {
+      "id": 101,
+      "title": "Computer Science",
+      "description": "Study of algorithms, programming, and computing systems."
+    },
+    {
+      "id": 102,
+      "title": " Cloud Computing Foundations ",
+      "description": "Learn the basics of storing and accessing data on the cloud."
+    },
+    {
+      "id": 103,
+      "title": "Software Engineering",
+      "description": "Application of engineering principles to software development."
+    },
+    {
+      "id": 104,
+      "title": "Web Development Essentials",
+      "description": "Learn the basics of building websites."
+    },
+    {
+      "id": 105,
+      "title": " Information Technology",
+      "description": "Protection of systems, networks, and programs from cyber threats."
+    }
+  ],
+  "instructors": [
+    {
+      "id": 1,
+      "name": "John Rey Silverio",
+      "subject": "Modern JavaScript & Next.js Prerequisites"
+    },
+    {
+      "id": 2,
+      "name": "Amor Santos",
+      "subject": "Data Science Fundamentals"
+    },
+    {
+      "id": 3,
+      "name": "Carlos  Dela cerna",
+      "subject": "Machine Learning Applications"
+    }
+  ]
+}
